e7661ecc9a50bec783b68f7eb68c0f470330d632
Desko Sekulović <desko.sekulovic@gmail.com>
2020-04-05 15:08:59 +0200

    Fetching data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { InputComponent } from './components/input/input.component';
-import { ListComponent } from './components/list/list.component';
+import { AppComponent } from "./app.component";
+import { InputComponent } from "./components/input/input.component";
+import { ListComponent } from "./components/list/list.component";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    InputComponent,
-    ListComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, InputComponent, ListComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-list',
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  selector: "app-list",
+  templateUrl: "./list.component.html",
+  styleUrls: ["./list.component.css"]
 })
 export class ListComponent implements OnInit {
+  data = "";
+  cities = ["London", "Novi Sad", "Vrbas"];
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onClick() {
+    this.data = "Clicked!!!";
+    this.cities.push("test");
   }
-
 }
