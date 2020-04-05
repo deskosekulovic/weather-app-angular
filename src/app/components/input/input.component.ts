c84a501bc1f5f44e09fa2b78cb2919ac078ae385
Desko Sekulović <desko.sekulovic@gmail.com>
2020-04-05 15:08:59 +0200

    Fetching data

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,23 +1,39 @@
 import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/operators";
 
 @Component({
   selector: "app-input",
   templateUrl: "./input.component.html",
-  styleUrls: ["./input.component.css"]
+  styleUrls: ["./input.component.css"],
 })
 export class InputComponent implements OnInit {
   public city = "";
-  constructor() {}
+  public data = [];
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {}
 
-  onChange(e, searchCity) {
+  onChange(e) {
+    this.city = e.target.value;
+    console.log(e);
     if (e.keyCode === 13 || e.keyCode === 188) {
-      this.city = searchCity
-        .trim()
-        .replace(",", "")
-        .toLowerCase();
+      this.city = e.target.value.trim().replace(",", "").toLowerCase();
       const url = `https://api.openweathermap.org/data/2.5/weather?appid=c3b8f2e28ea9bf6f5d8bcb678001ab74&units=metric&q=${this.city}`;
+      this.http
+        .get<any>(url)
+        .pipe(
+          map((data) => {
+            return {
+              temp: data.main.temp,
+              weather: data.weather[0].main,
+              humidity: data.main.humidity,
+              wind: data.wind.speed,
+            };
+          })
+        )
+        .subscribe((data) => this.data.push(data));
+      this.city = "";
     }
   }
 }
